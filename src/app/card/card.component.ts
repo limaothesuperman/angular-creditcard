c7ff11020db5c7d159cb5528023776736ea2f34f
Mao Li <mli94@stevens.edu>
2023-06-13 15:06:35 -0500

    finished project

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+
+  @Input() cardNumber?: string | null;
+  @Input() name?: string | null;
+  @Input() expiration?: string | null;
+
+}
